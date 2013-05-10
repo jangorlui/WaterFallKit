ce3ad5f6915025f06ad47794394da0c730b65637
mac <mac@macs-MacBook-Pro.local>
2013-05-10 15:17:07 +0800

    Initial Commit

---
+++ WaterFallKit/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  WaterFallKit
+//
+//  Created by mac on 5/10/13.
+//  Copyright (c) 2013 AlvinLui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ WaterFallKit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WaterFallKit
+//
+//  Created by mac on 5/10/13.
+//  Copyright (c) 2013 AlvinLui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
